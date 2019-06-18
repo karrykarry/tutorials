f82ef21b7ada73c5cff38cdfe89208a986d55a06
kari <12kari20@gmail.com>
2019-06-18 15:54:02 +0900

    new

--- intution_deeplearning/ch03/cifar-10.py
+++ intution_deeplearning/ch03/cifar-10.py
@@ -1,4 +1,4 @@
-#$B:G=i4|$N(BCNN$B$G$"$j!"C1=c$J2hA|$NJQ7A$dOD$_$K4h7r$H$$$&(BCNN$B$N4pK\E*$JFC@-$r(BMNIST$B$N2hA|G'<1%?%9%/$K$*$$$F>ZL@$7$?$N$,(BLeNet
+#最初期のCNNであり、単純な画像の変形や歪みに頑健というCNNの基本的な特性をMNISTの画像認識タスクにおいて証明したのがLeNet
 import os
 import keras
 from keras.models import Sequential
@@ -7,48 +7,52 @@ from keras.layers.convolutional import MaxPooling2D
 from keras.layers.core import Activation
 from keras.layers.core import Flatten, Dropout
 from keras.layers.core import Dense
-from keras.datasets import mnist
-from keras.optimizers import Adam
-from keras.callbacks import TensorBoard
+from keras.datasets import cifar10
+from keras.optimizers import RMSprop
+# from keras.optimizers import Adam
+from keras.callbacks import TensorBoard, ModelCheckpoint
 
-####### $B%M%C%H%o!<%/(B ######
+####### ネットワーク ######
 
-def lenet(input_shape, num_classes):
+def network(input_shape, num_classes):
     model = Sequential()
 #extract image features by convolution and max pooling layers
-    model.add(Conv2D(20, kernel_size=5, padding="same",
+    model.add(Conv2D(32, kernel_size=5, padding="same",
         input_shape=input_shape, activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2))) #$B=hM}8e$NFCD'%^%C%W$O(B14x14x20
-    model.add(Conv2D(50, kernel_size=5, padding="same",
+    model.add(MaxPooling2D(pool_size=(2, 2))) #処理後の特徴マップは14x14x20
+    model.add(Dropout(0.25))
+    model.add(Conv2D(64, kernel_size=3, padding="same",
         activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2))) #$B=hM}8e$NFCD'%^%C%W$O(B7x7x20
+    model.add(MaxPooling2D(pool_size=(2, 2))) #処理後の特徴マップは7x7x20
 # classify the class by fully-connected layers
     model.add(Flatten())
-    model.add(Dense(500, activation="relu"))
+    model.add(Dense(512, activation="relu"))
+    model.add(Dropout(0.5))
     model.add(Dense(num_classes))
-    model.add(Activation("softmax")) #$B%=%U%H%^%C%/%94X?t$rE,MQ$9$k$3$H$GCM$r(B0~1$B$N3NN(CM$KJQ99$7$^$9!#(B
+    model.add(Activation("softmax")) #ソフトマックス関数を適用することで値を0~1の確率値に変更します。
     return model
 
 ##########################
 
-####### $B3X=,$5$;$k$?$a$N%G!<%?%;%C%H$rMQ0U$7$^$9!#(B ########
-# $B:#2s$O(BMNIST$B$N<j=q$-?t;z$rMQ0U$7$^$9!#(B
+####### 学習させるためのデータセットを用意します。 ########
+# 今回はMNISTの手書き数字を用意します。
 
-class MNISTDataset():
+class CIFAR10Dataset():
 
     def __init__(self):
-        self.image_shape = (28, 28, 1) # image is 28x28x1 (grayscale)
+        self.image_shape = (32, 32, 3) # image is 28x28x1 (grayscale)
         self.num_classes = 10
 
     def get_batch(self):
-        (x_train, y_train), (x_test, y_test) = mnist.load_data()
+        # (x_train, y_train), (x_test, y_test) = mnist.load_data()
+        (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
         x_train, x_test = [self.preprocess(d) for d in [x_train, x_test]]
         y_train, y_test = [self.preprocess(d, label_data=True) for d in [y_train, y_test]]
 
         return x_train, y_train, x_test, y_test
 
-# $B2hA|%G!<%?$O(Bpreprocess$B$K$h$j(B0~1$B$NCM$KJQ49$7!#@52r%G!<%?(B($B%i%Y%k%G!<%?(B)$B$O(Bone-hot$B%Y%/%H%k$KJQ49$7$^$9!#(B
+# 画像データはpreprocessにより0~1の値に変換し。正解データ(ラベルデータ)はone-hotベクトルに変換します。
 #
     def preprocess(self, data, label_data=False):
     
@@ -65,8 +69,8 @@ class MNISTDataset():
 
 #############################################################
 
-# $B%M%C%H%o!<%/$r3X=,$5$;$k(B Trainer $B$r<BAu$7$^$9!#(B
-#$B3X=,$5$;$k(Bmodel$B!":G>.2=$9$Y$-8m:9$G$"$k(Bloss$B!":GE,$NJ}K!$G$"$k(Boptimizer$B$r<u$1<h$j!"(Btrain$B$G3X=,%G!<%?$r<u$1<h$j3X=,$5$;$^$9!#(B
+# ネットワークを学習させる Trainer を実装します。
+#学習させるmodel、最小化すべき誤差であるloss、最適の方法であるoptimizerを受け取り、trainで学習データを受け取り学習させます。
 
 class Trainer():
 
@@ -75,38 +79,45 @@ class Trainer():
         self._target.compile(loss=loss, optimizer=optimizer, metrics=["accuracy"])
         self.verbose = 1
         self.log_dir = os.path.join(os.path.dirname(__file__), "logdir")
+        self.model_file_name = "model_file.hdf5"
 
     def train(self, x_train, y_train, batch_size, epochs, validation_split):
         if os.path.exists(self.log_dir):
             import shutil
             shutil.rmtree(self.log_dir) # remove previous execution
-
             os.mkdir(self.log_dir)
 
         self._target.fit(
             x_train, y_train,
             batch_size=batch_size, epochs=epochs,
             validation_split=validation_split,
-            callbacks=[TensorBoard(log_dir=self.log_dir)],
+            callbacks=[
+                TensorBoard(log_dir=self.log_dir),
+                ModelCheckpoint(os.path.join(self.log_dir, self.model_file_name),
+                    save_best_only=True)
+                ],
             verbose=self.verbose
         )
 
 
 #############################################
 
-####### $B:G8e$KDj5A$7$?=hM}$r8F$S=P$7<B:]$K3X=,$r9T$&=hM}$r<BAu$7$^$9!#(B
+####### 最後に定義した処理を呼び出し実際に学習を行う処理を実装します。
 
 
-dataset = MNISTDataset()
+# dataset = MNISTDataset()
+dataset = CIFAR10Dataset()
 
 
 # make model
-model = lenet(dataset.image_shape, data.num_classes)
+# model = lenet(dataset.image_shape, data.num_classes)
+model = network(dataset.image_shape, data.num_classes)
 
 #train the model
 x_train, y_train, x_test, y_test = dataset.get_batch()
 
-trainer = Trainer(model, loss="categorical_crossentropy", optimizer=Adam())
+# trainer = Trainer(model, loss="categorical_crossentropy", optimizer=Adam())
+trainer = Trainer(model, loss="categorical_crossentropy", optimizer=RMSprop())
 
 trainer.train(x_train, y_train, batch_size=128, epochs=12, validation_split=0.2)
 
@@ -114,5 +125,6 @@ trainer.train(x_train, y_train, batch_size=128, epochs=12, validation_split=0.2)
 score = model.evaluate(x_test, y_test, verbose=0)
 print("Test loss:", score[0])
 print("Test accuracy:", score[1])
+
 
 
